6985eced3dedd02fe9bf7ab976c014883209d080
Revathi Praba K <159789940+Revathi196@users.noreply.github.com>
2025-02-02 22:42:45 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+// Example for adding a smooth scroll feature (can be expanded)
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
